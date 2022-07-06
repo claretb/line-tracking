9815988d8d41301ef727abfc8cebf22be1673309
Talha <talhayranci@gmail.com>
2022-07-06 18:53:07 +0100

    Changes for robustness.

--- src/App.js
+++ src/App.js
@@ -28,59 +28,38 @@ function App() {
 
   }
 
-  var firstImage = null;
-  var left = 0;
-  var centre = 0;
-  var right = 0;
-  var decision = null;
-
-  function startProcess() {
+  async function startProcess() {
     var canvas = document.getElementById("myCanvas");
-    // var ghostCanvas = document.getElementById("ghostCanvas");
-
+    var ctx = canvas.getContext("2d");
+    var ghostCtx = ghostCanvas.getContext("2d");
     var videoWidth = webcamRef.current.video.videoWidth;
     var videoHeight = webcamRef.current.video.videoHeight;
 
-    canvas.width = videoWidth;
-    canvas.height = videoHeight;
-    ghostCanvas.width = videoWidth;
-    ghostCanvas.height = videoHeight;
-
-    var ctx = canvas.getContext("2d");
-    var ghostCtx = ghostCanvas.getContext("2d");
-    ctx.clearRect(
-      0,
-      0,
-      videoWidth,
-      videoHeight
-    );
-    ghostCtx.clearRect(
-      0,
-      0,
-      videoWidth,
-      videoHeight
-    );
+    if (canvas.width != videoWidth || canvas.height != videoHeight) {
+      canvas.width = videoWidth;
+      canvas.height = videoHeight;
+      ghostCanvas.width = videoWidth;
+      ghostCanvas.height = videoHeight;
+    }
 
     const imageSrc = webcamRef.current.getScreenshot();
     var image = new Image;
-    image.src = imageSrc;
-    if (firstImage == null) {
-      firstImage = image;
-    }
 
-    ghostCtx.filter = 'grayscale(1)';
-    ghostCtx.drawImage(image, 0, 0);
+    image.onload = function () {
+      ghostCtx.clearRect(
+        0,
+        0,
+        videoWidth,
+        videoHeight
+      );
 
-    // var grayscaleImageData = ghostCtx.getImageData(0, 0, videoWidth, videoHeight);
+      ghostCtx.filter = 'grayscale(1)';
+      ghostCtx.drawImage(image, 0, 0);
 
-    var newLeft = getAverage(ghostCtx.getImageData(0, videoHeight - 150, (videoWidth / 3), 150));
-    var newCentre = getAverage(ghostCtx.getImageData((videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150));
-    var newRight = getAverage(ghostCtx.getImageData(2 * (videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150));
-
-    if (newLeft != 0 || newRight != 0 || newCentre != 0) {
-      left = newLeft;
-      right = newRight;
-      centre = newCentre;
+      var left = getAverage(ghostCtx.getImageData(0, videoHeight - 150, (videoWidth / 3), 150));
+      var centre = getAverage(ghostCtx.getImageData((videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150));
+      var right = getAverage(ghostCtx.getImageData(2 * (videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150));
+      var decision = "NO WAY!";
 
       if (centre < 90) {
         decision = "FORWARD";
@@ -91,34 +70,41 @@ function App() {
       else if (right < 90) {
         decision = "RIGHT";
       }
-      else {
-        decision = "NO WAY!";
+
+      ctx.clearRect(
+        0,
+        0,
+        videoWidth,
+        videoHeight
+      );
+
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "red";
+
+      ctx.strokeRect(0, videoHeight - 150, (videoWidth / 3), 150);
+      ctx.strokeRect((videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150);
+      ctx.strokeRect(2 * (videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150);
+
+      ctx.font = "50px Arial";
+      ctx.fillStyle = "red";
+
+      ctx.fillText(left, (videoWidth * 3 / 30), videoHeight - 55);
+      ctx.fillText(centre, (videoWidth * 13 / 30), videoHeight - 55);
+      ctx.fillText(right, (videoWidth * 23 / 30), videoHeight - 55);
+
+      if (decision != null) {
+
+        ctx.font = "70px Arial";
+        ctx.fillStyle = "green";
+
+        ctx.fillText(decision, (videoWidth / 3), videoHeight - 200);
       }
+
+      setTimeout(() => startProcess(), 10);
     }
 
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "red";
+    image.src = imageSrc;
 
-    ctx.strokeRect(0, videoHeight - 150, (videoWidth / 3), 150);
-    ctx.strokeRect((videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150);
-    ctx.strokeRect(2 * (videoWidth / 3), videoHeight - 150, (videoWidth / 3), 150);
-
-    ctx.font = "50px Arial";
-    ctx.fillStyle = "red";
-
-    ctx.fillText(left, (videoWidth * 3 / 30), videoHeight - 55);
-    ctx.fillText(centre, (videoWidth * 13 / 30), videoHeight - 55);
-    ctx.fillText(right, (videoWidth * 23 / 30), videoHeight - 55);
-
-    if (decision != null) {
-
-      ctx.font = "70px Arial";
-      ctx.fillStyle = "green";
-
-      ctx.fillText(decision, (videoWidth / 3), videoHeight - 200);
-    }
-
-    setTimeout(() => startProcess(), 1);
   };
 
   return (
@@ -158,14 +144,6 @@ function App() {
           Start
         </Button>
       </div>
-      {/* <div style={{ marginTop: "1200px"  }}>
-        <canvas
-          id="ghostCanvas"
-          width={720}
-          height={1200}
-          style={{ backgroundColor: "blue" }}
-        />
-      </div> */}
     </div>
   );
 }
